1a9e4e48118d1037ab16246c086e614ff9eff0f7
Артём <artem@MacBook-Air-Artem.local>
2026-07-10 18:56:08 +0300

    Some changes. Automatic download models, creating folders. Added README

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import whisper
 import os
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).resolve().parent
+
+# создание нужных папок
+def check_and_create_folders(current_dir):
+    REQUIRED_FOLDERS = ['models', 'input', 'results']
+
+    for folder_name in REQUIRED_FOLDERS:
+        folder_path = current_dir / folder_name
+        folder_path.mkdir(exist_ok=True)
+
+
+# проверка есть модель в папке или нет
+def check_model(model_name, current_dir):
+    folder_path = current_dir / 'models'
+    file_path = folder_path / f'{model_name}.pt'
+    return file_path.is_file()
+
+
+# проход по всем моделям
+# если нет в папке - скачивание
+def load_models(current_dir):
+    models = ['tiny', 'base']
+        # 'small': '244 M',
+        # 'medium': '769 M',
+        # 'large': '1550 M',
+        # 'turbo': '809 M'
+
+    model_directory = current_dir / 'models'
+    for model_name in models:
+        if not check_model(model_name, current_dir): 
+            whisper.load_model(model_name, download_root=str(model_directory))
+    
 
 # выбор модели
-def model_choice():
-    all_models_path = './models/'
-    all_models_list = os.listdir(all_models_path)
+def model_choice(current_dir):
+    folder_path = current_dir / 'models'
+    all_models_list = [f for f in os.listdir(folder_path) if f.endswith('.pt')]
 
-    for model_index in range(len(all_models_list)):
-        model_num = model_index + 1
-        print(model_num, all_models_list[model_index])
+    for index, model_name in enumerate(all_models_list):
+        print(f"{index+1}. {model_name}")
+    
     model_index = int(input('Выбери модель по номеру в списке (рекомендуемая - small)\n'))
     model_path = './models/' + all_models_list[model_index - 1]
     return model_path
@@ -45,12 +79,14 @@ def create_file(audio_name, result):
     print('Готово! Транскрипция файла в папке "results".')
 
 def transcription():
+    check_and_create_folders(CURRENT_DIR)
+    load_models(CURRENT_DIR)
 
     try:
-        model_path = model_choice()
+        model_path = model_choice(CURRENT_DIR)
     except ValueError:
         print('Неправильный ввод. Попробуйте еще раз')
-        model_path = model_choice()
+        model_path = model_choice(CURRENT_DIR)
 
     try:
         audio_name, audio_path = file_choice()
@@ -63,10 +99,12 @@ def transcription():
 
 def main():
     try:
-        transcription()
+        transcription()  
     except KeyboardInterrupt:
         print('Завершение работы..')
         return
+    except Exception as e:
+        print(f'Произошла ошибка -> {e}\nПроверьте наличие программы ffmpeg на компьютере\nУстановите по ссылке -> https://www.ffmpeg.org/download.html\n')
 
 if __name__ == '__main__':
     main()
